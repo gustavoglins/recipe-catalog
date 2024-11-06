e503321b00f37b6d58e37228e38cd416e8dc90da
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-11-05 21:05:52 -0300

    init commit

---
+++ src/main/java/com/cookify/CookifyApplication.java
@@ -0,0 +1,13 @@
+package com.cookify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookifyApplication.class, args);
+	}
+
+}
